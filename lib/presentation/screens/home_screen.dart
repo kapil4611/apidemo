d8cd46fed527d9c858cfd45a8c3070bea1a6c2de
kapil4611 <kapil14516@gmail.com>
2026-03-11 11:35:03 +0530

    refressh users functionality

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -36,78 +36,87 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Users")),
-      body: Column(
-        children: [
-          /// SEARCH BAR
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              onChanged: filterUsers,
-              decoration: InputDecoration(
-                hintText: "Search user...",
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          context.read<UserBloc>().add(GetUsersEvent());
+        },
+        child: Column(
+          children: [
+            /// SEARCH BAR
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                onChanged: filterUsers,
+                decoration: InputDecoration(
+                  hintText: "Search user...",
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: BlocBuilder<UserBloc, UserState>(
-              builder: (context, state) {
-                if (state is UserLoadingState) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (state is UserErrorState) {
-                  return Center(child: Text(state.message));
-                }
-                if (state is UserLoadedState) {
-                  allUsers = state.users;
-                  filteredUsers = filteredUsers.isEmpty
-                      ? allUsers
-                      : filteredUsers;
-                  if (filteredUsers.isEmpty) Center(child: Text("No user..."));
-                  return ListView.builder(
-                    itemCount: filteredUsers.length,
-                    itemBuilder: (context, index) {
-                      User user = filteredUsers[index];
-                      return ListTile(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => UserDetailScreen(user),
+            Expanded(
+              child: BlocBuilder<UserBloc, UserState>(
+                builder: (context, state) {
+                  if (state is UserLoadingState) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (state is UserErrorState) {
+                    return Center(child: Text(state.message));
+                  }
+                  if (state is UserLoadedState) {
+                    allUsers = state.users;
+                    filteredUsers = filteredUsers.isEmpty
+                        ? allUsers
+                        : filteredUsers;
+                    if (filteredUsers.isEmpty)
+                      Center(child: Text("No user..."));
+                    return ListView.builder(
+                      itemCount: filteredUsers.length,
+                      itemBuilder: (context, index) {
+                        User user = filteredUsers[index];
+                        return ListTile(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UserDetailScreen(user),
+                              ),
+                            );
+                          },
+                          leading: CircleAvatar(
+                            child: Image.network(
+                              user.avatar!,
+                              loadingBuilder:
+                                  (context, child, loadingProgress) {
+                                    if (loadingProgress == null) return child;
+
+                                    return Center(
+                                      child: CircularProgressIndicator(),
+                                    );
+                                  },
+
+                              errorBuilder: (context, error, stackTrace) {
+                                return const Icon(
+                                  Icons.broken_image,
+                                  size: 50,
+                                  color: Colors.grey,
+                                );
+                              },
                             ),
-                          );
-                        },
-                        leading: CircleAvatar(
-                          child: Image.network(
-                            user.avatar!,
-                            loadingBuilder: (context, child, loadingProgress) {
-                              if (loadingProgress == null) return child;
-
-                              return Center(child: CircularProgressIndicator());
-                            },
-
-                            errorBuilder: (context, error, stackTrace) {
-                              return const Icon(
-                                Icons.broken_image,
-                                size: 50,
-                                color: Colors.grey,
-                              );
-                            },
                           ),
-                        ),
-                        title: Text("${user.firstName}"),
-                      );
-                    },
-                  );
-                }
-                return Container();
-              },
+                          title: Text("${user.firstName}"),
+                        );
+                      },
+                    );
+                  }
+                  return Container();
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
